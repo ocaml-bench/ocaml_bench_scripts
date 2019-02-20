087149f6f2eec9e8dfa94e2b95bb27a1af85fa82
Tom Kelly <ctk21@cl.cam.ac.uk>
2019-02-20 11:58:59 +0000

    skip build stage if binary exists

--- run_backfill.py
+++ run_backfill.py
@@ -33,7 +33,7 @@ args = parser.parse_args()
 def shell_exec(cmd, verbose=args.verbose, check=False, capture_output=False):
 	if verbose:
 		print('+ %s'%cmd)
-	
+
 	if capture_output:
 		return subprocess.run(cmd, shell=True, check=check, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	else:
@@ -63,7 +63,7 @@ shell_exec('git checkout %s'%args.branch)
 if args.commit_choice_method == 'version_tags':
 	proc_output = shell_exec('git log --pretty=format:\'%%H %%s\' | grep VERSION | grep %s'%args.branch, capture_output=True)
 	hash_comments = proc_output.stdout.decode('utf-8').strip().split('\n')[::-1]
-	
+
 	hashes = [hc.split(' ')[0] for hc in hash_comments]
 	if args.verbose:
 		for hc in hash_comments:
@@ -81,7 +81,7 @@ elif args.commit_choice_method == 'status_success':
 			curl_xtra_args = curl_xtra_args + ' -H "Authorization: token %s"'%args.github_oauth_token
 		proc_output = shell_exec('curl %s https://api.github.com/repos/ocaml/ocaml/commits/%s/status | jq .state'%(curl_xtra_args, h), capture_output=True)
 		return proc_output.stdout.decode('utf-8').strip().strip('"')
-		
+
 	hashes = []
 	for h in all_hashes:
 		state = get_hash_status(h)
@@ -107,30 +107,33 @@ for (n, h) in enumerate(hashes):
 	hashdir = os.path.join(outdir, h)
 	if args.verbose: print('processing to %s'%hashdir)
 	shell_exec('mkdir -p %s'%hashdir)
-	
+
 	## run build for commit
 	builddir = os.path.join(hashdir, 'ocaml_build')
 	build_context_fname = os.path.join(builddir, 'build_context.conf')
 	if 'build' in args.run_stages:
-		log_fname = os.path.join(hashdir, 'build_%s.log'%run_timestamp) 
-		completed_proc = shell_exec('%s/build_ocaml_hash.py --repo %s -j %d %s %s %s &> %s'%(SCRIPTDIR, args.repo, args.jobs, h, builddir, verbose_args, log_fname))
-		if completed_proc.returncode != 0:
-			print('ERROR[%d] in build_ocaml_hash for %s (see %s)'%(completed_proc.returncode, h, log_fname))
-			continue
+		if os.path.isfile(os.path.join(builddir, 'bin', 'ocaml')):
+			print('Skipping build for %s as already built'%h)
+		else:
+			log_fname = os.path.join(hashdir, 'build_%s.log'%run_timestamp)
+			completed_proc = shell_exec('%s/build_ocaml_hash.py --repo %s -j %d %s %s %s &> %s'%(SCRIPTDIR, args.repo, args.jobs, h, builddir, verbose_args, log_fname))
+			if completed_proc.returncode != 0:
+				print('ERROR[%d] in build_ocaml_hash for %s (see %s)'%(completed_proc.returncode, h, log_fname))
+				continue
 
-		# output build context
-		build_context = {
-			'commitid': h, 
-			'branch': args.branch,
-			'project': 'ocaml',
-			'executable': 'vanilla', 
-		}
-		write_context(build_context, build_context_fname)
+			# output build context
+			build_context = {
+				'commitid': h,
+				'branch': args.branch,
+				'project': 'ocaml',
+				'executable': 'vanilla',
+			}
+			write_context(build_context, build_context_fname)
 
 	## run operf for commit
 	operf_micro_dir = os.path.join(hashdir, 'operf-micro')
 	if 'operf' in args.run_stages:
-		log_fname = os.path.join(hashdir, 'operf_%s.log'%run_timestamp) 
+		log_fname = os.path.join(hashdir, 'operf_%s.log'%run_timestamp)
 		completed_proc = shell_exec('%s/run_operf_micro.py %s %s --operf_binary %s %s &> %s'%(SCRIPTDIR, os.path.join(builddir, 'bin'), operf_micro_dir, OPERF_BINARY, verbose_args, log_fname))
 		if completed_proc.returncode != 0:
 			print('ERROR[%d] in run_operf_micro for %s (see %s)'%(completed_proc.returncode, h, log_fname))
@@ -138,14 +141,14 @@ for (n, h) in enumerate(hashes):
 
 		# output run context
 		run_context = {
-			'environment': args.environment, 
+			'environment': args.environment,
 		}
 		write_context(run_context, os.path.join(operf_micro_dir, 'run_context.conf'))
 		shell_exec('cp %s %s'%(build_context_fname, os.path.join(operf_micro_dir, 'build_context.conf')))
 
 	## upload commit
 	if 'upload' in args.run_stages:
-		upload_fname = os.path.join(hashdir, 'upload_%s.log'%run_timestamp) 
+		upload_fname = os.path.join(hashdir, 'upload_%s.log'%run_timestamp)
 		shell_exec('%s/load_operf_data.py %s %s &> %s'%(SCRIPTDIR, operf_micro_dir, verbose_args, log_fname))
 		if completed_proc.returncode != 0:
 			print('ERROR[%d] in load_operf_data for %s (see %s)'%(completed_proc.returncode, h, upload_fname))
