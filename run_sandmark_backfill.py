7fd3eeae34664c2cc2d4bf46ca64b2c9d4f4bc8b
Shakthi Kannan <author@shakthimaan.com>
2020-03-23 15:36:04 +0530

    Support use of a specific tag release

--- run_sandmark_backfill.py
+++ run_sandmark_backfill.py
@@ -54,6 +54,7 @@ parser.add_argument('--archive_dir', type=str, help='location to make archive (c
 parser.add_argument('--upload_project_name', type=str, help='specific upload project name (default is ocaml_<branch name>', default=None)
 parser.add_argument('--upload_date_tag', type=str, help='specific date tag to upload', default=None)
 parser.add_argument('--codespeed_url', type=str, help='codespeed URL for upload', default=CODESPEED_URL)
+parser.add_argument('--tag_commit', type=str, help="use specific tag release only", default=None)
 parser.add_argument('-v', '--verbose', action='store_true', default=False)
 
 args = parser.parse_args()
@@ -152,20 +153,24 @@ def check_archive_dir(d):
     return True
 archive_dirs = [f for f in archive_dirs if check_archive_dir(f)]
 
-## generate list of hash commits
-hashes = git_hashes.get_git_hashes(args)
+hashes = []
 
-if args.incremental_hashes:
-    def check_hash_new(h):
-        hash_dir = os.path.join(outdir, h)
-        hash_already_run = os.path.exists(hash_dir)
-        if args.verbose and hash_already_run:
-            print('Found results at %s skipping rerun'%hash_dir)
-        return not hash_already_run
+if args.tag_commit is None:
+	## generate list of hash commits
+	hashes = git_hashes.get_git_hashes(args)
 
-    hashes = [h for h in hashes if check_hash_new(h)]
+	if args.incremental_hashes:
+		def check_hash_new(h):
+			hash_dir = os.path.join(outdir, h)
+			hash_already_run = os.path.exists(hash_dir)
+			if args.verbose and hash_already_run:
+				print('Found results at %s skipping rerun'%hash_dir)
+				return not hash_already_run
 
-hashes = hashes[-args.max_hashes:]
+	hashes = [h for h in hashes if check_hash_new(h)]
+	hashes = hashes[-args.max_hashes:]
+else:
+	hashes = [args.tag_commit]
 
 if args.verbose:
     print('Found %d hashes using %s to do %s on'%(len(hashes), args.commit_choice_method, args.run_stages))
